694c400eee5cf6b732489a13510214055f633197
Nicole Kozak <nicole.kozak@escola.pr.gov.br>
2025-07-04 10:57:39 -0300

    dcfdcf

--- main.js
+++
@@ -1,9 +0,0 @@
-
-
-function inserirNome () {
-    let nomeUsuario = prompt("informe o seu nome:");
-    let elemento = document.querySelector ("#nome-usuario");
-    elemento.textContent = nomeUsuario;
-}
-
-inserirNome ()
